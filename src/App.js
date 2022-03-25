2180f94c1fedfd9907ec9e6c0915c9eea1db255a
sazidahmednassir <sazidahmednassir@gmail.com>
2022-03-25 09:36:13 -0800

    four commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Card from './Component/Card/Card';
 import Navbar from './Component/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <div class="container">
+  <div class="row ">
+    <div class="col-md-10 mt-2">
+    <Card></Card>
+    </div>
+    <div class="col-md-2">
+      Second in DOM, unordered
+    </div>
+   
+  </div>
+</div>
     </div>
   );
 }
---
+++ src/Component/Card/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Card.css";
+
+const Card = () => {
+    return (
+        <div>
+            <div class="card" className="cardwidth" >
+  <img src="https://m.media-amazon.com/images/I/61J18Xn9bfS._AC_SX569_.jpg" class="card-img-top" alt="..."></img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Card;
