d847a81e45e904171373d59738fc963dd3accc00
sazidahmednassir <sazidahmednassir@gmail.com>
2022-03-25 11:50:28 -0800

    seven commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './Component/Card/Card';
 import Navbar from './Component/Navbar/Navbar';
 
 function App() {
+  const [cameras, setCamera] = useState([])
+
+  useEffect(()=>{
+    fetch("data.json")
+    .then(res=>res.json())
+    .then(data=>setCamera(data))
+  },[])
   return (
     <div className="App">
       <Navbar></Navbar>
       <div class="container-fluid">
   <div class="row ">
     <div class="col-md-10 mt-2">
-    <Card></Card>
+     <div class="container d-flex flex-wrap justify-content-between align-items-end mt-3">{cameras.map((camera) => (
+          <Card camera={camera} key={camera.id} ></Card> 
+        ))}</div>
+    
+     
+        
+      
+      
+
+  
     </div>
     <div class="col-md-2 bg-primary">
       Second in DOM, unordered
--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -3,15 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import "./Card.css";
 
-const Card = () => {
+const Card = (props) => {
+    const {name, img, price, category}= props.camera;
     return (
         <div>
             <div class="card" className="cardwidth" >
-  <img src="https://m.media-amazon.com/images/I/61J18Xn9bfS._AC_SX569_.jpg" class="card-img-top" alt="camera"></img>
+  <img src={img} class="card-img-top" alt="camera"></img>
   <div class="card-body">
-  <h5 class="card-title text-dark font-weight-bold">Canon</h5>
-    <p class="card-text text-danger  font-weight-bold">$1249</p>
-    <p class="card-text text-danger  font-weight-bold">camera</p>
+  <h5 class="card-title text-dark font-weight-bold">{name}</h5>
+    <p class="card-text text-danger  font-weight-bold">${price}</p>
+    <p class="card-text text-danger  font-weight-bold">{category}</p>
     <button  class="btn btn-danger " > Add to Cart
     <FontAwesomeIcon icon={faShoppingCart} className="product-btn"></FontAwesomeIcon>
      
