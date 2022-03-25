4f589046711b313e7e4868851a77b9a7029da934
sazidahmednassir <sazidahmednassir@gmail.com>
2022-03-25 08:50:50 -0800

    second commit

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-md ">
+    <p  class="fs-1 text-danger  font-weight-bold">Camera Shop</p>
+  </div>
+</nav>
+        </div>
+    );
+};
+
+export default Navbar;
