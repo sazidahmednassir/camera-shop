41c6dbc91c15b95cf58cee50c5da2dd36fef2c72
sazidahmednassir <sazidahmednassir@gmail.com>
2022-03-25 11:15:12 -0800

    six commit

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -1,3 +1,5 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import "./Card.css";
 
@@ -5,11 +7,15 @@ const Card = () => {
     return (
         <div>
             <div class="card" className="cardwidth" >
-  <img src="https://m.media-amazon.com/images/I/61J18Xn9bfS._AC_SX569_.jpg" class="card-img-top" alt="..."></img>
+  <img src="https://m.media-amazon.com/images/I/61J18Xn9bfS._AC_SX569_.jpg" class="card-img-top" alt="camera"></img>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  
+  <h5 class="card-title text-dark font-weight-bold">Canon</h5>
+    <p class="card-text text-danger  font-weight-bold">$1249</p>
+    <p class="card-text text-danger  font-weight-bold">camera</p>
+    <button  class="btn btn-danger " > Add to Cart
+    <FontAwesomeIcon icon={faShoppingCart} className="product-btn"></FontAwesomeIcon>
+     
+    </button>
   </div>
 </div>
         </div>
