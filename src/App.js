2704d6a23115922adc01d9ecd3c767ffc16934b8
sazidahmednassir <sazidahmednassir@gmail.com>
2022-03-25 12:49:53 -0800

    eight commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './Component/Card/Card';
+import Cart from './Component/Cart/Cart';
 import Navbar from './Component/Navbar/Navbar';
 
 function App() {
@@ -16,20 +17,13 @@ function App() {
       <Navbar></Navbar>
       <div class="container-fluid">
   <div class="row ">
-    <div class="col-md-10 mt-2">
-     <div class="container d-flex flex-wrap justify-content-between align-items-end mt-3">{cameras.map((camera) => (
+  <div class="col-md-2 bg-secondary text-white order-md-2 sticky-top">
+     <Cart></Cart>
+    </div>
+    <div class="col-md-10 mt-2 ">
+     <div class="container d-flex flex-wrap justify-content-between align-items-end mt-3 order-md-1">{cameras.map((camera) => (
           <Card camera={camera} key={camera.id} ></Card> 
         ))}</div>
-    
-     
-        
-      
-      
-
-  
-    </div>
-    <div class="col-md-2 bg-primary">
-      Second in DOM, unordered
     </div>
    
   </div>
---
+++ src/Component/Cart/Cart.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./Cart.css";
+
+const Cart = () => {
+    return (
+        <div>
+<div class="card text-white bg-primary mb-3" className="cart-width">
+ 
+  <div class="card-body d-flex">
+  <img src="https://m.media-amazon.com/images/I/61J18Xn9bfS._AC_SX569_.jpg" class="card-img-top" className="box"   alt="..."></img>
+    <h5 class="card-title">Name: Canon</h5>
+    
+  </div>
+  <div class="d-flex flex-column ">
+  <button  class="btn btn-danger  mt-2" > change
+     
+     </button>
+     <button  class="btn btn-danger mt-2" > Delete
+      
+     </button>
+  </div>
+ 
+</div>
+        </div>
+    );
+};
+
+export default Cart;
